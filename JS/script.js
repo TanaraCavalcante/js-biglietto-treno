387c3a498c39e6dfa84f7502be203d7b6d1dfa41
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-07-31 15:55:18 -0400

    price variable

--- JS/script.js
+++ JS/script.js
@@ -10,3 +10,10 @@ console.log(totalKm)
 //Chiedi all'utente la sua età
 const age = prompt('Quanti anni hai?', 31)
 console.log(age)
+
+/*---------------------------
+      FASI DI ELABORAZIONE
+----------------------------*/
+//Preparo della variabile PREZZO per Km
+const priceKm = parseInt(0.89)
+console.log (priceKm, typeof priceKm)
