4bca57c9d7324f91fde2350e270045798ccf15dd
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-07-31 15:36:27 -0400

    data collect

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,12 @@
-console.log ("java ok")
+
+
+/*--------------------------
+       RACCOLTA DATI
+--------------------------*/
+//Chiedi all'utente quanti km vuole percorrere
+const totalKm = prompt('Quanti KM vuoi percorrere?', 21)
+console.log(totalKm)
+
+//Chiedi all'utente la sua età
+const age = prompt('Quanti anni hai?', 31)
+console.log(age)
